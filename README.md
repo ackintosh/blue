a88afedc49aab273bde9e5ebc93f05f46a6987ec
akihito.nakano <sora.akatsuki@gmail.com>
2019-08-08 08:14:03 +0900

    Add sub-command

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 ## Run Genesis Core Node
 
 ```bash
-./blue {port}
+./blue core {port}
 ```
 
 ```bash
-$ cargo run 7878
+$ cargo run core 7878
 Bootstrapping as a GENESIS Core Node...
 Initializing core node...
 Initializing connection manager...
@@ -17,11 +17,11 @@ Listening on 127.0.0.1:7878
 ## Run Core Node
 
 ```bash
-./blue {port} {port num of genesis core node}
+./blue core {port} {port num of genesis core node}
 ```
 
 ```bash
-$ cargo run 17879 7878
+$ cargo run core 17879 7878
 Bootstrapping as a Core Node...
 Initializing core node...
 Sending message: Message { type: Add, source_port: "17879" }
--- src/main.rs
+++ src/main.rs
@@ -4,23 +4,42 @@ use blue::node::Node;
 fn main() {
     let args: Vec<String> = std::env::args().collect();
     println!("command line arguments: {:?}", args);
+    let r#type = &args[1];
 
+    if r#type == "core" {
+        run_core_node(args);
+    } else if r#type == "edge" {
+        run_edge_node(args)
+    } else {
+        println!("Please specify the node type as first argument: `core` or `edge`");
+        std::process::exit(1);
+    }
+}
+
+fn run_core_node(args: Vec<String>) {
     match args.len() {
-        2 => {
+        3 => {
             println!("Bootstrapping as a GENESIS Core Node...");
-            let port = &args[1];
+            let port = &args[2];
             let mut core = GenesisCoreNode::new("127.0.0.1".to_owned(), port.clone());
             core.start();
         }
-        3 => {
+        4 => {
             println!("Bootstrapping as a Core Node...");
 
-            let port = &args[1];
-            let genesis_node = Node("127.0.0.1".to_owned(), args[2].clone());
+            let port = &args[2];
+            let genesis_node = Node("127.0.0.1".to_owned(), args[3].clone());
 
             let mut core = CoreNode::new("127.0.0.1".to_owned(), port.clone(), genesis_node);
             core.start();
         }
-        _ => println!("[Usage]\r ./blue port [genesis-node]\r\r port: A port number what node should listen on.\r genesis-node(optional): A port number of GENESIS node.")
+        _ => {
+            println!("[Usage]\r ./blue core {{port}} [{{genesis-node}}]\r\r port: A port number what node should listen on.\r genesis-node(optional): A port number of GENESIS node.");
+            std::process::exit(1);
+        }
     }
+}
+
+fn run_edge_node(args: Vec<String>) {
+
 }
